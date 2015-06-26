2738db9656da94963142c94a03d4eb6caaaaf2bd
Roy Chiang <roy@Roys-MacBook-Pro.local>
2015-06-26 14:37:40 +0800

    added contact to statics

--- public/dist/application.js
+++ public/dist/application.js
@@ -1366,11 +1366,18 @@ angular.module('statics').config(['$stateProvider',
 	function($stateProvider) {
 		// Statics state routing
 		$stateProvider.
-		state('statics', {
+		state('aboutUs', {
 			url: '/aboutus',
-			templateUrl: 'modules/statics/views/statics.client.view.html',
+			templateUrl: 'modules/statics/views/about.statics.client.view.html',
 			data: {
 				pageTitle: 'About Us'
+			}
+		}).
+		state('contactUs', {
+			url: '/contact',
+			templateUrl: 'modules/statics/views/contact.statics.client.view.html',
+			data: {
+				pageTitle: 'Contact us'
 			}
 		});
 	}
